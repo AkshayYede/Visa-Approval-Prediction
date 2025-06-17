9a943acb4dcd9b42d241897f1739459026ac91e7
AkshayYede <akshayyede16@gmail.com>
2025-06-17 13:16:35 +0530

    template, setup, requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akshay Yede",
+    author_email="akshayyede16@gmail.com",
+    packages=find_packages()
+)
